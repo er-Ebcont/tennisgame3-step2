4ffb47913255fbb7fa9f5631f8587d9cae8cf104
Emre Rauhofer <emre.rauhofer.extern@orfnet.at>
2023-10-13 13:48:53 +0200

    Removed player 1 score var

--- Tennis/TennisGame3.cs
+++ Tennis/TennisGame3.cs
@@ -3,7 +3,7 @@ namespace Tennis
     public class TennisGame3 : ITennisGame
     {
         private int playerScore2;
-        private int playerScore1;
+        //private int playerScore1;
         private IPlayer player1;
         private IPlayer player2;
         private string[] scoreTerms = { "Love", "Fifteen", "Thirty", "Forty" };
@@ -16,26 +16,26 @@ namespace Tennis
 
         public string GetScore()
         {
-            if ((playerScore1 < 4 && playerScore2 < 4) && (playerScore1 + playerScore2 < 6))
+            if ((this.player1.Score < 4 && playerScore2 < 4) && (this.player1.Score + playerScore2 < 6))
             {
-                string scoreTermPlayer1 = scoreTerms[playerScore1];
-                return (playerScore1 == playerScore2) ? scoreTermPlayer1 + "-All" : scoreTermPlayer1 + "-" + scoreTerms[playerScore2];
+                string scoreTermPlayer1 = scoreTerms[this.player1.Score];
+                return (this.player1.Score == playerScore2) ? scoreTermPlayer1 + "-All" : scoreTermPlayer1 + "-" + scoreTerms[playerScore2];
             }
 
-            if (playerScore1 == playerScore2)
+            if (this.player1.Score == playerScore2)
             {
                 return "Deuce";
             }
                 
-            string winningPlayerName = playerScore1 > playerScore2 ? this.player1.Name : this.player2.Name;
-            return ((playerScore1 - playerScore2) * (playerScore1 - playerScore2) == 1) ? "Advantage " + winningPlayerName : "Win for " + winningPlayerName;
+            string winningPlayerName = this.player1.Score > playerScore2 ? this.player1.Name : this.player2.Name;
+            return ((this.player1.Score - playerScore2) * (this.player1.Score - playerScore2) == 1) ? "Advantage " + winningPlayerName : "Win for " + winningPlayerName;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == this.player1.Name)
             {
-                this.playerScore1 += 1;
+                this.player1.Score += 1;
             }
 
             if (playerName == this.player2.Name)
