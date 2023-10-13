583511a74411f68541976a549030ac05184fd3e5
Emre Rauhofer <emre.rauhofer.extern@orfnet.at>
2023-10-13 14:40:57 +0200

    Added score class

---
+++ Tennis/Score.cs
@@ -0,0 +1,15 @@
+﻿namespace Tennis
+{
+    public class Score
+    {
+        private int _score;
+        private string[] scoreTerms = { "Love", "Fifteen", "Thirty", "Forty" };
+
+        public Score(int score)
+        {
+            _score = score;
+        }
+
+        public int ScoreProp { get => _score; set => _score = value; }
+    }
+}
