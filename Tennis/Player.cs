55c53cee5afa71d0943b28cb0e8395c9732ec40d
Emre Rauhofer <emre.rauhofer.extern@orfnet.at>
2023-10-13 13:42:02 +0200

    Added properties to player class

--- Tennis/Player.cs
+++ Tennis/Player.cs
@@ -10,5 +10,8 @@
             this.playerName1 = playerName1;
             this.playerScore1 = playerScore1;
         }
+
+        public string PlayerName1 { get => playerName1; set => playerName1 = value; }
+        public int PlayerScore1 { get => playerScore1; set => playerScore1 = value; }
     }
 }
